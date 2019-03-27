2394af7e4ced75889859f4f1bba134984906ae26
mun <quangnd.edu@gmail.com>
2019-03-27 17:10:08 +0700

    Add multiple-pointers

---
+++ proplem-solving-patterns/multiple-pointers/sumZero_refactored.js
@@ -0,0 +1,21 @@
+// See the naive version first please
+
+// -- Refactored version --
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
